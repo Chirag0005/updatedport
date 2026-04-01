75093e70fddf78cb73553703b2092d37af7e2321
Chirag <143305907+Chirag0005@users.noreply.github.com>
2026-04-01 14:44:35 +0530

    deploy

--- test_build.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const result = execSync('npm run build', { encoding: 'utf8' });
-  console.log('SUCCESS:', result);
-} catch (err) {
-  console.log('FAILED STDOUT:\\n', err.stdout);
-  console.log('FAILED STDERR:\\n', err.stderr);
-}
